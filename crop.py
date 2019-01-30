b1afb1a21f878e4a40469570baedc46ad3615c39
Kyle McDonald <kyle@kylemcdonald.net>
2019-01-29 21:10:12 -0500

    add safe_crop

--- crop.py
+++ crop.py
@@ -1,5 +1,26 @@
 import numpy as np
 
+def safe_crop(arr, n, s, w, e, fill=128):
+    shape = np.asarray(arr.shape)
+    shape[:2] = s - n, e - w
+    no, so, wo, eo = 0, shape[0], 0, shape[1]
+    if n < 0:
+        no += -n
+        n = 0
+    if w < 0:
+        wo += -w
+        w = 0
+    if s >= arr.shape[0]:
+        so -= s - arr.shape[0]
+        s = arr.shape[0]
+    if e >= arr.shape[1]:
+        eo -= e - arr.shape[1]
+        e = arr.shape[1]
+    out = np.empty(shape, dtype=arr.dtype)
+    out.fill(fill)
+    out[no:so,wo:eo] = arr[n:s,w:e]
+    return out
+
 def inner_square_crop(img):
     size = np.asarray(img.shape[:2])
     min_side = min(size)
