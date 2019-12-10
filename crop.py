4ef7b702e29d334e98ac31c99f868e9389728e51
Kyle McDonald <kyle@kylemcdonald.net>
2019-12-09 18:07:50 -0800

    place_inside

--- crop.py
+++ crop.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def safe_crop(arr, tblr, fill=128):
+def safe_crop(arr, tblr, fill=None):
     n,s,w,e = tblr
     shape = np.asarray(arr.shape)
     shape[:2] = s - n, e - w
@@ -17,13 +17,31 @@ def safe_crop(arr, tblr, fill=128):
     if e >= arr.shape[1]:
         eo -= e - arr.shape[1]
         e = arr.shape[1]
+    cropped = arr[n:s,w:e]
+    if fill is None:
+        return cropped
     out = np.empty(shape, dtype=arr.dtype)
     out.fill(fill)
     try:
-        out[no:so,wo:eo] = arr[n:s,w:e]
+        out[no:so,wo:eo] = cropped
     except ValueError:
         # this happens when there is no overlap
         pass
+    return out
+
+def place_inside(img, xy, output_shape, fill=0):
+    ix,iy = xy
+    ih,iw,ic = img.shape
+    oh,ow = output_shape
+    out = np.empty((oh,ow,ic), dtype=arr.dtype)
+    out.fill(fill)
+    ox = 0
+    oy = 0
+    if ih < oh and iy > 0:
+        oy = oh - ih
+    if iw < ow and ix > 0:
+        ox = ow - iw
+    out[oy:oy+ih,ox:ox+iw] = img
     return out
 
 def inner_square_crop(img):
