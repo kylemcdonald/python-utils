76282862212f5dcf86caedb1f5a1bcccd612d42c
Kyle McDonald <kyle@kylemcdonald.net>
2020-02-15 20:27:28 -0800

    rename list_all_files to list_files

--- list_files.py
+++ list_files.py
@@ -1,6 +1,7 @@
 import os
 import fnmatch
 
+# does not operate recursively
 def list_directories(directory):
     for f in os.listdir(directory):
         joined = os.path.join(directory, f)
@@ -8,10 +9,12 @@ def list_directories(directory):
             yield joined
 
 # extensions can be a string or list, can include the preceding . or not
-def list_all_files(directory, extensions=None, exclude_prefixes=('__', '.')):
+# operates recursively
+def list_files(directory, extensions=None, exclude_prefixes=('.',)):
     if type(extensions) == str:
         extensions = [extensions]
-    extensions = [('' if e.startswith('.') else '.') + e for e in extensions]
+    if extensions is not None:
+        extensions = [('' if e.startswith('.') else '.') + e for e in extensions]
     for root, dirnames, filenames in os.walk(directory):
         filenames = [f for f in filenames if not f.startswith(exclude_prefixes)]
         dirnames[:] = [d for d in dirnames if not d.startswith(exclude_prefixes)]
