e4634d80cc1a94f1a4c77126ab286f9843ae562a
Kyle McDonald <kyle@kylemcdonald.net>
2020-11-15 01:21:27 -0800

    allow more args to ffmpeg

--- ffmpeg.py
+++ ffmpeg.py
@@ -162,21 +162,23 @@ def vidread(fn, samples=None, rate=None, hwaccel=None):
     proc.wait()
 
 class VideoWriter:
-    def __init__(self, fn, vcodec='libx264', fps=60, in_pix_fmt='rgb24', out_pix_fmt='yuv420p'):
+    def __init__(self, fn, vcodec='libx264', fps=60, in_pix_fmt='rgb24', out_pix_fmt='yuv420p', input_args=None, output_args=None):
         self.fn = fn
-        self.vcodec = vcodec
-        self.fps = fps
         self.process = None
-        self.in_pix_fmt = in_pix_fmt
-        self.out_pix_fmt = out_pix_fmt
+        self.input_args = {} if input_args is None else input_args
+        self.output_args = {} if output_args is None else output_args
+        self.input_args['framerate'] = fps
+        self.input_args['pix_fmt'] = in_pix_fmt
+        self.output_args['pix_fmt'] = out_pix_fmt
+        self.output_args['vcodec'] = vcodec
     
     def add(self, frame):
         if self.process is None:
             h,w = frame.shape[:2]
             self.process = (
                 ffmpeg
-                    .input('pipe:', format='rawvideo', pix_fmt=self.in_pix_fmt, s='{}x{}'.format(w, h), framerate=self.fps)
-                    .output(self.fn, pix_fmt=self.out_pix_fmt, vcodec=self.vcodec)
+                    .input('pipe:', format='rawvideo', s='{}x{}'.format(w, h), **self.input_args)
+                    .output(self.fn, **self.output_args)
                     .overwrite_output()
                     .run_async(pipe_stdin=True)
             )
