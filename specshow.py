fb21b85f880edf4983ae1772a79a43c96a6e9d5b
Kyle McDonald <kyle@kylemcdonald.net>
2020-11-15 01:23:30 -0800

    refactor spacing as hop_length

--- specshow.py
+++ specshow.py
@@ -3,9 +3,9 @@ import matplotlib.pyplot as plt
 from utils.imutil import imshow
 
 # expects output directly from librosa.stft or librosa.cqt
-def specshow(x, sr=44100, spacing=256, max_frames=1960, skip=1, gamma=6, use_mag=True, cmap='inferno', zoom=None, show=True):
+def specshow(x, sr=44100, hop_length=512, max_frames=1960, skip=1, gamma=6, use_mag=True, cmap='inferno', zoom=None, show=True):
     bins, frames = x.shape
-    seconds = (frames * spacing) / sr
+    seconds = (frames * hop_length) / sr
     minutes = int(seconds // 60)
     seconds = seconds - (minutes * 60)
     sx = x[:,:max_frames*skip:skip]
@@ -22,6 +22,6 @@ def specshow(x, sr=44100, spacing=256, max_frames=1960, skip=1, gamma=6, use_mag
     img = 255 * np.flipud(spec)
     if show:
         imshow(img, retina=zoom is None, zoom=zoom)
-        print(f'{minutes}:{seconds:04.2f} @ {sr}Hz, {frames} frames x {bins} bins @ {spacing} spacing')
+        print(f'{minutes}:{seconds:04.2f} @ {sr}Hz, {frames} frames x {bins} bins @ {hop_length} hop_length')
     else:
         return img
