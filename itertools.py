201fa4c423ad1f8258f0bc1fbb8f2f2d433aa4fd
Kyle McDonald <kyle@kylemcdonald.net>
2021-01-13 21:55:03 -0800

    more flexible iteration

--- itertools.py
+++ itertools.py
@@ -1,4 +1,6 @@
 import numpy as np
+from itertools import islice
+from itertools import chain
 
 def np_chunks(x, chunk_size):
     chunk_count = len(x)//chunk_size
@@ -7,5 +9,12 @@ def np_chunks(x, chunk_size):
     return x[:n].reshape(chunk_count, chunk_size, *shape)
 
 def chunks(x, n):
-    for i in range(0, len(x), n):
-        yield x[i:i+n]
+    # return slices of lists
+    if hasattr(x, '__len__'):
+        for i in range(0, len(x), n):
+            yield x[i:i+n]
+    else:
+        # return sub-generators of generators
+        i = iter(x)
+        for e in i:
+            yield chain([e], islice(i, n-1))
