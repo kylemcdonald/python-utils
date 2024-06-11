108c489c08fe705859dd327a9fd62bd83161650c
Kyle McDonald <kyle@kylemcdonald.net>
2024-06-10 20:25:44 -0700

    update memoize

--- memoize.py
+++ memoize.py
@@ -1,14 +1,16 @@
 import hashlib
 import pickle
 import inspect
+import os
 
 def memoize(func):
-    is_method = 'self' in inspect.getfullargspec(func).args
+    is_method = 'self' in inspect.getfullargspec(func).args    
     def wrapper(*args, **kwargs):
         args_to_hash = args[1:] if is_method else args
-        h = hashlib.md5(str(args_to_hash).encode()).hexdigest()
+        h = hashlib.md5(str(args_to_hash).encode()).hexdigest()        
         fn = f"cache/{func.__name__}_{h}.pkl"
         try:
+            os.makedirs('cache', exist_ok=True)
             with open(fn, 'rb') as f:
                 return pickle.load(f)
         except FileNotFoundError:
